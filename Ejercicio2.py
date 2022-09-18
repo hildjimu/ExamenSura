830b664afbdee78df0244a7b4e8563bcb115d8cb
Hilda Magaly Jimenez Muñoz <hjimenez@sura.com.co>
2022-09-18 12:32:52 -0500

    Ejercicio 2 del examén

---
+++ Ejercicio2.py
@@ -0,0 +1,18 @@
+'''
+2.Leer información de 10 frutas {nombre, color, precio} para preparar
+un salpicón; el programa debe permitir mostrar las 10 frutas ingresadas
+al mismo tiempo+(1)
+'''
+print("    Preparando Salpicones Maga    ")
+
+frutas=[]
+
+for i in range(10):
+    fruta={}
+    fruta['nombre']=input("Ingrese el nombre de la fruta: ")
+    fruta['color']=input("Ingrese el color de la fruta: ")
+    fruta['precio']=input("Ingrese el precio de la fruta: ")
+    frutas.append(fruta)
+    
+print(frutas)
+
